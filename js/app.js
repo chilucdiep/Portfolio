6c8b33f493ee38d2760f39ba61e22ecfb0546efa
Chi-Luc <chilucdiep@hotmail.com>
2021-01-14 00:02:00 -0500

    Change file reference

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 new hoverEffect({
     parent: document.querySelector(".distortion"),
     intensity: 0.2,
-    image1: "../images/memoji.svg",
-    image2: "../images/aboutImage.svg",
-    displacementImage: "../images/hoverDiss.png"
+    image1: "images/memoji.svg",
+    image2: "images/aboutImage.svg",
+    displacementImage: "images/hoverDiss.png"
 });
